fe886683b43c7267842eef5a947c52df3ba207c5
Aarush Sinha <sinhaaarush11@gmail.com>
2023-03-31 12:57:45 +0530

    Update

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -133,7 +133,7 @@ function LandingPage({ loggedIn, setLoggedIn }) {
         </Title>
         <Button.Group className={classes.ButtonGroup}>
           <Button onClick={() => googlelogin()} leftIcon={<IconBrandGoogle />}>
-            Goooglee Login
+            Goooglee Login Please
           </Button>
         </Button.Group>
       </div>
