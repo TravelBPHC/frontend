7b76088a435185613041101ed74ad402ee13f59d
Aarush Sinha <sinhaaarush11@gmail.com>
2023-03-31 13:08:55 +0530

    Update

--- src/index.js
+++ src/index.js
@@ -85,13 +85,13 @@ serviceWorkerRegistration.register({
     if (waitingServiceWorker) {
       waitingServiceWorker.addEventListener("statechange", (event) => {
         if (event.target.state === "activated") {
-          if (
-            window.confirm(
-              "There is a new version of the app ready. Please reload to update."
-            )
-          ) {
-            window.location.reload();
-          }
+          // if (
+          //   window.confirm(
+          //     "There is a new version of the app ready. Please reload to update."
+          //   )
+          // ) {
+          window.location.reload();
+          // }
         }
       });
       waitingServiceWorker.postMessage({ type: "SKIP_WAITING" });
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -133,7 +133,7 @@ function LandingPage({ loggedIn, setLoggedIn }) {
         </Title>
         <Button.Group className={classes.ButtonGroup}>
           <Button onClick={() => googlelogin()} leftIcon={<IconBrandGoogle />}>
-            Goooglee Login Pleaaaa
+            Google Login
           </Button>
         </Button.Group>
       </div>
